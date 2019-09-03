7306803e7810742bb1128c3718f2c1611af36139
Richipic <richipic86@libero.it>
2019-09-03 23:28:50 +0200

    aggiunta main. makefile e libreria ptask

---
+++ main.c
@@ -0,0 +1,20 @@
+//------------------------------------------------
+// POOL GAME
+// Created by Stefano Rossi and Riccardo Piccolo
+//------------------------------------------------
+
+
+#include "ptask.h"
+#include <allegro.h>
+#include <pthread.h>
+
+int main(void) {
+  
+
+  while (!key[KEY_ESC]){
+  }
+    ;
+  allegro_exit();
+
+  return 0;
+}
