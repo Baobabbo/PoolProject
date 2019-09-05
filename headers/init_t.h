2362465a4be16e88fe86af5088c11d3d5aa80fa5
Richipic <richipic86@libero.it>
2019-09-05 02:30:24 +0200

    init_t.c
    aggiunta dell inizializzazione di Allegro, dei semafori necessari.
    caricamento immagini del tavolo da biliardo e della stecca .

--- headers/init_t.h
+++ headers/init_t.h
@@ -18,9 +18,27 @@
 #define RESX 1280       // game resolution X
 #define RESY 720        // game resolution Y
 
+//  goblas variable
+
+extern sem_t semball[]; // semaphore for balls
+extern sem_t semuser;   // semaphore for user
+extern sem_t semmain;   // main semaphore
+
+
+extern int user_id, graphics_id; // ids for user and graphic tasks
+
+
+// bitmaps for game elements
+extern BITMAP *bground;
+extern BITMAP *cue;
+extern BITMAP *balls[NUM_BALLS];
+extern BITMAP *buf;
+
 // tasks
 extern void ball_task(void);
 extern void graphics_task(void);
 extern void user_task(void);
+
+extern void init();
 
 #endif // INIT_T_H
---
+++ src/init_t.c
@@ -0,0 +1,65 @@
+//------------------------------------------------
+//  INIT_T.C - load images and initialization of tasks
+//------------------------------------------------
+#include "init_t.h"
+
+BITMAP *bground;
+BITMAP *cue;
+BITMAP *balls[NUM_BALLS];
+BITMAP *buf;
+
+int graphics_id, user_id;
+sem_t semball[NUM_BALLS];
+sem_t semmain;
+sem_t semuser;
+
+
+// initialization biliard cue
+void init_cue(void) {
+  cue = load_bitmap("img/stick_ok.bmp", NULL);
+  if (cue == NULL) {
+    printf("ERROR: CUE not found\n");
+    exit(1);
+  }
+}
+
+// initialization game table
+void init_bground(void) {
+  BITMAP *tmp;
+  tmp = load_bitmap("img/pool_table.bmp", NULL);
+  if (tmp == NULL) {
+    printf("ERROR: BACKGROUND not found\n");
+    exit(1);
+  }
+
+  bground = create_bitmap(RESX, RESY);
+  buf = create_bitmap(RESX, RESY);
+  stretch_sprite(bground, tmp, 0, 0, RESX, RESY);
+}
+
+// initialization of allegro parameters,semaphore and game
+void init() {
+
+  allegro_init();
+  set_color_depth(desktop_color_depth());
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED, RESX, RESY, 0, 0);
+  install_keyboard();
+  install_mouse();
+  show_mouse(screen);
+  
+  init_cue();
+  init_bground();
+  
+
+  //  initialization of tasks with deadline
+  ptask_init(SCHED_OTHER, GLOBAL, PRIO_INHERITANCE);
+
+  
+
+  //  semaphore initialization
+  for (int i = 0; i < NUM_BALLS; ++i) {
+    sem_init(&semball[i], 0, 1);
+  }
+  sem_init(&semmain, 0, 1);
+  sem_init(&semuser, 0, 0);
+}
