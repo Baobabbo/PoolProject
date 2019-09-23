091c9913cb5ffb82f55e41241e020fd652ee9ff1
Baobabbo <33123672+Baobabbo@users.noreply.github.com>
2019-09-23 19:16:26 +0200

    Aggiunta Funzionalità
    I task palla e grafico sono stati dotati di funzionalità

--- src/ball_t.c
+++ src/ball_t.c
@@ -4,9 +4,27 @@
 
 #include "init_t.h"
 
+// Ball pasition and movement update function
+
+void update_ball(void){
+	// stuff
+}
+
+// Body of ball processes
+
 void ball_task(void){
-	// task initialization
-	// ...
-	// continuous update of task position
-	// ...
+	// Task initialization
+	int task_index;
+	sem_wait(&semmain);
+	task_index = inde;
+	inde++;
+	sem_post(&semmain);
+	
+	// Continuous update of task position
+	
+	while(1){
+		// aggiornamenti sicuri!
+		// ...
+		ptask_wait_for_period();
+	}
 }
--- src/graphics_t.c
+++ src/graphics_t.c
@@ -5,5 +5,12 @@
 #include "init_t.h"
 
 void graphics_task(void) {
-	// roba
+	// Screen refresh at each execution
+	
+	while(1){
+		blit(bground, buf, 0, 0, RESX / 2 - bground->w / 2, RESY - bground->h,
+			RESX, RESY);
+		
+		ptask_wait_for_period();
+	}
 }
