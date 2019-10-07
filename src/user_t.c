737ee8de29dbd0b5f387defef00064d9be6f61a8
Richipic <richipic86@libero.it>
2019-10-07 14:16:31 +0200

    click mode

--- src/user_t.c
+++ src/user_t.c
@@ -4,12 +4,40 @@
 
 #include "init_t.h"
 
-// Body of the user process
+int m_x, m_y;         // mouse position
+char mode;            // function in mode
+char click;           // user command
 
+void init_user(void) {
+  m_x = 0;
+  m_y = 0;
+  mode = 0;
+  click = 0;
+  
+}
+
+// Body of the user process
+// user chooses the angle and shooting power
 void user_task(void) {
 	// Inizializzazione del task TODO
-	
-	ptask_wait_for_activation();
+
+  ptask_set_priority(ptask_get_index(), 90);
+  init_user();
+  ptask_wait_for_activation();
+  while (1) {
+    if (click) {
+      if (!(mouse_b & 1))
+        click = 0;
+    }
+    if (mouse_b & 1) {
+      if (!click)
+        mode++;
+      click = 1;
+    }
+    if (mouse_b & 2) {
+      mode = 0;
+      
+    }
 	
 	// Gestione Input TODO
 }
